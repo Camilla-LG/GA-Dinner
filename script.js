edfc4690af63a4512a08272558b09df9a35c9f02
camillalo1 <camillalo@getacademy.no>
2023-09-04 18:30:31 +0200

    Update script.js
    functions

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
-let dinnerContainer = "";
-let enterDinner = "";
+let dinnerContainer = [];
 let dinnerSuggestion = "";
 let searchBar = "";
-let dinnerOutput = "";
+let viewDinner = "";
 
 let dinnerHelp = document.getElementById("dinner");
 
 updateView();
 function updateView(){
-    dinnerHelp.innerHTML+= /*HTML*/`
+    dinnerHelp.innerHTML= /*HTML*/`
     
     <h1> DINNER TIME! </h1>
     <h3> Help Wanted - Help Given </h3>
 
-    <input type="text" id="enterDinner">
+    <input type="text" id="enterDinner" placeholder="Enter Suggestion...">
+    <button id="inputClick" onclick="addDinner()"> Add this Dish </button>
 
     <h5> Need a Dinner Suggestion? </h5>
-    <div id="dinnerOutput"></div>
-    <button id="helpMe"> Help! </button>
+    <div id="output"> ${viewDinner} </div>
+    <button id="helpMe" onclick="dinnerTip()"> Help! </button>
 
-    ;`
+    `;
+}
+
+function addDinner(){
+    let enterDinnerValue = document.getElementById('enterDinner').value;
+    dinnerContainer.push(enterDinnerValue);
+    document.getElementById('enterDinner').value="";
+}
+
+function dinnerTip(){
+    document.getElementById('output');
+    let randomIndex = Math.floor(Math.random() * dinnerContainer.length);
+    viewDinner = dinnerContainer[randomIndex];
+    updateView();
 }
